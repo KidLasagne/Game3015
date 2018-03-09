b2f9fc2c4b4943ae2b1e30fcc53b4a7704b0e04b
Sym66 <squee669@gmail.com>
2018-03-08 23:37:35 -0500

    Simple Translation Added
    Simple Translation Added

---
+++ Juggernaut Engine/Source/Subterfuge/Subterfuge/GameObjectManager.cpp
@@ -0,0 +1,76 @@
+#include "GameObjectManager.h"
+
+
+GameObjectManager::GameObjectManager()
+{
+}
+
+
+GameObjectManager::~GameObjectManager()
+{
+}
+
+void GameObjectManager::Awake()
+{
+	for (auto& game_object : GameObjectLibrary)
+	{
+		game_object->Awake();
+	}
+}
+
+void GameObjectManager::Start()
+{
+	for (auto& game_object : GameObjectLibrary)
+	{
+		game_object->Start();
+	}
+}
+
+void GameObjectManager::Update()
+{
+	for (auto& game_object : GameObjectLibrary)
+	{
+		game_object->Update();
+	}
+}
+
+void GameObjectManager::LateUpdate()
+{
+	for (auto& game_object : GameObjectLibrary)
+	{
+		game_object->LateUpdate();
+	}
+}
+
+void GameObjectManager::PushGameObject(GameObject* gam)
+{
+	GameObjectLibrary.push_back(gam);
+}
+
+GameObject* GameObjectManager::FindGameObjectByName(std::string nam)
+{
+	for (auto& game_object : GameObjectLibrary)
+	{
+		if (game_object->GetName() == nam)
+		{
+			return game_object;
+		}
+	}
+
+	return NULL;
+}
+
+std::vector<GameObject*> GameObjectManager::FindGameObjectsByName(std::string nam)
+{
+	std::vector<GameObject*> ObjList;
+
+	for (auto& game_object : GameObjectLibrary)
+	{
+		if (game_object->GetName() == nam)
+		{
+			ObjList.push_back(game_object);
+		}
+	}
+
+	return ObjList;
+}
--- Juggernaut Engine/Source/Subterfuge/Subterfuge/main.cpp
+++ Juggernaut Engine/Source/Subterfuge/Subterfuge/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include "GameObject.h"
+#include "GameObjectManager.h"
 
 int main()
 {
@@ -25,15 +26,52 @@ int main()
 	GameObject *gam1 = new GameObject();
 	GameObject *gam2 = new GameObject();
 	GameObject *gam3 = new GameObject();
+	GameObject *gam4 = new GameObject();
+
+	gam1->SetRotation(-90.0f);
+	gam2->SetWorldPosition(100,0);
+	gam2->SetSphereColor(sf::Color::Red);
+	gam3->SetWorldPosition(200, 0);
+	gam3->SetSphereColor(sf::Color::Green);
+	gam4->SetWorldPosition(300, 0);
+	gam4->SetSphereColor(sf::Color::Yellow);
 
 	gam1->SetName("g1");
 	gam2->SetName("g2");
 	gam3->SetName("g3");
+	gam4->SetName("g3");
 
 	gam1->AttachChild(gam2);
-	gam1->AttachChild(gam3);
+	gam2->AttachChild(gam3);
+	gam3->AttachChild(gam4);
 
-	std::cout << "Object Name: " << gam1->FindObjectByName("g3")->GetName() << std::endl;
+	GameObjectManager *gamu = new GameObjectManager();
+	gamu->PushGameObject(gam1);
+	gamu->PushGameObject(gam2);
+	gamu->PushGameObject(gam3);
+	gamu->PushGameObject(gam4);
+
+	std::cout << "Searching GameObject by name:" << std::endl;
+
+	if (gamu->FindGameObjectByName("g2") != NULL)
+	{
+		std::cout << "I found the GameObject you were looking for, it's name is: " << gamu->FindGameObjectByName("g2")->GetName() << std::endl;
+	}
+
+	std::vector<GameObject*> tempList = gamu->FindGameObjectsByName("g3");
+
+	std::cout << "Searching for multiple GameObjects: " << std::endl;
+
+	int i = 1;
+
+	for (auto& game_object : tempList)
+	{
+		std::cout << i << ": This GameObject is named: " << game_object->GetName() << std::endl;
+		i++;
+	}
+
+	std::cout << "Testing for NULL: " << std::endl;
+	//std::cout << "Object Name: " << gam1->FindObjectByName("g3")->GetName() << std::endl;
 	if (gam1->FindObjectByName("powerman") == NULL)
 	{
 		std::cout << "No Object Found." << std::endl;
@@ -43,6 +81,10 @@ int main()
 		std::cout << "Found it!" << std::endl;
 	}
 
+	gamu->Awake();
+	gamu->Start();
+
+	float f = 0.0f;
 
 	while (window.isOpen())
 	{
@@ -53,7 +95,23 @@ int main()
 			{
 				window.close();
 			}
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				gamu->FindGameObjectByName("g1")->worldTransform.Position.x += 50.0f;
+			}
 		}
+
+		gamu->FindGameObjectByName("g2")->SetRotation(50.0f);
+
+		gamu->Update();
+		gamu->LateUpdate();
+
+		window.clear();
+		for (auto& game_object : gamu->GameObjectLibrary)
+		{
+			window.draw(game_object->GetSphere());
+		}
+		window.display();
 
 		//window.clear();
 		//window.draw(sprite);
