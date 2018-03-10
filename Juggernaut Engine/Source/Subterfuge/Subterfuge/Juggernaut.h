82561e0b20a66bb5c8d88f823702827bab7b2e7f
Sym66 <squee669@gmail.com>
2018-03-10 02:02:08 -0500

    User Input and Transformation
    User Input and Transformation.

--- Juggernaut Engine/Source/Subterfuge/Subterfuge/Juggernaut.h
+++ Juggernaut Engine/Source/Subterfuge/Subterfuge/Juggernaut.h
@@ -13,6 +13,7 @@ public:
 	void MainLoop();
 	GameObjectManager GetGameObjectManager() { return Manager; }
 	void RenderTheWindow();
+	void UserInput(float x, float y, GameObject *First, GameObject *Second, GameObject *Third, GameObject *Fourth, int MoveWhich);
 	//GameObject* SpawnNewObject(float newX, float newY, float scale, std::string name, sf::Color fillCol, float radius);
 	//GameObject* SpawnNewObject();
 
